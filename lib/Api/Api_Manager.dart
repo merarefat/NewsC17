275f25e6dd74da359d6d2cef451042e35e1c73be
Mera Refat <merarefat@gmail.com>
2026-03-28 16:15:38 +0200

    add folderHome and Categoy_details + file category_details

--- lib/Api/Api_Manager.dart
+++ lib/Api/Api_Manager.dart
@@ -1,10 +1,31 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:news/Api/Api_Constants.dart';
+import 'package:news/Api/end_points.dart';
+import 'package:news/Model/Source_Respon.dart';
 
 class ApiManager {
   /*  https://newsapi.org/v2/top-headlines/sources?apiKey=20adf6b9f2cf4be9961c33d604ef4fcf */
-  static void getSources() {
-    Uri url = Uri.https(ApiConstants.baseUrl,);
-    http.get(url);
+  static Future<SourceRespon> getSources() async {
+    // http response
+    Uri url = Uri.https(
+        ApiConstants.baseUrl, // اسم ال server
+        EndPoints.sourceApi,
+        { // query parameters as map
+          'apiKey': ApiConstants.apiKey});
+    Response response = await http.get(url);
+    // SourceRespon.fromJson(jsonDecode(response.body));
+    try {
+      var responseBody = response.body; //String
+      // string => json
+      dynamic json = jsonDecode(responseBody); //json
+      // json => object
+      return SourceRespon.fromJson(json); //object
+    } catch (e) {
+      rethrow;
+    }
+
   }
 }
--- lib/Api/end_points.dart
+++ lib/Api/end_points.dart
@@ -1,1 +1,3 @@
-class ApiName {}
+class EndPoints {
+  static const String sourceApi = '/v2/top-headlines/sources';
+}
